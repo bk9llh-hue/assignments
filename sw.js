676972a04ffd280357ab9ec98f1df4193aa30be0
bk9llh-hue <bk9llh@gmail.com>
2025-11-23 20:13:27 -0600

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,18 +1,8 @@
-// sw.js - simple service worker
-const CACHE = 'classroom-proxy-v1';
-const ASSETS = ['/', '/browser.html', '/home.html'];
-self.addEventListener('install', (e) => {
-e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)));
-self.skipWaiting();
+self.addEventListener('install', e => {
+  console.log('Service Worker installed');
+  self.skipWaiting();
 });
-self.addEventListener('activate', (e) => {
-e.waitUntil(self.clients.claim());
-});
-self.addEventListener('fetch', (e) => {
-const url = new URL(e.request.url);
-if (url.pathname === '/' || url.pathname === '/browser.html' || url.pathname === '/home.html') {
-e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request)));
-return;
-}
-e.respondWith(fetch(e.request).catch(()=>caches.match(e.request)));
+
+self.addEventListener('fetch', e => {
+  e.respondWith(fetch(e.request));
 });
