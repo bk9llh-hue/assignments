00bc82bb2eb05243a31ce2e5b917921b9ebaaf76
bk9llh-hue <bk9llh@gmail.com>
2025-11-23 20:05:50 -0600

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,28 +1,18 @@
-const CACHE_NAME = 'smart-browser-v1';
-const urlsToCache = [
-    '/',
-    '/browser.html',
-    '/index.html'
-];
-
-self.addEventListener('install', e=>{
-    e.waitUntil(
-        caches.open(CACHE_NAME).then(cache=>cache.addAll(urlsToCache))
-    );
+// sw.js - simple service worker
+const CACHE = 'classroom-proxy-v1';
+const ASSETS = ['/', '/browser.html', '/home.html'];
+self.addEventListener('install', (e) => {
+e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)));
+self.skipWaiting();
 });
-
-self.addEventListener('fetch', e=>{
-    e.respondWith(
-        caches.match(e.request).then(response=>{
-            return response || fetch(e.request);
-        })
-    );
+self.addEventListener('activate', (e) => {
+e.waitUntil(self.clients.claim());
 });
-
-self.addEventListener('activate', e=>{
-    e.waitUntil(
-        caches.keys().then(keys=>Promise.all(
-            keys.filter(key=>key!==CACHE_NAME).map(key=>caches.delete(key))
-        ))
-    );
+self.addEventListener('fetch', (e) => {
+const url = new URL(e.request.url);
+if (url.pathname === '/' || url.pathname === '/browser.html' || url.pathname === '/home.html') {
+e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request)));
+return;
+}
+e.respondWith(fetch(e.request).catch(()=>caches.match(e.request)));
 });
